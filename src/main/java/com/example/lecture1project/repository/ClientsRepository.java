930ddf8d49a6b168fdb40764ece503e254435720
aigulsharip <aigul.sharip@nu.edu.kz>
2021-10-25 02:08:02 +0600

    Files for practice2 was uploaded

--- src/main/java/com/example/lecture1project/repository/ClientsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lecture1project.repository;
-
-import com.example.lecture1project.database.Client;
-
-import java.util.List;
-
-public interface ClientsRepository {
-    public List<Client> findClients();
-}
--- src/main/java/com/example/lecture1project/repository/TasksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lecture1project.repository;
-
-import com.example.lecture1project.database.Service;
-
-import java.util.List;
-
-public interface TasksRepository {
-    public List<Service> findTasks();
-}
