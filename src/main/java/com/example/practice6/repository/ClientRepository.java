2016e728ccaa569c101b560440e42379c1c878b3
aigulsharip <aigul.sharip@nu.edu.kz>
2021-11-13 01:39:54 +0600

    Lafayette project is completed

--- src/main/java/com/example/practice6/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.practice6.repository;
-
-import com.example.practice6.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
